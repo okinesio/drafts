e8560dffb082271ca2b8286bc9fa69198ee9e801
emnullfuenf <emnullfuenf@googlemail.com>
2015-11-27 15:44:29 +0100

    BMI160 Breakout Code

---
+++ Arduino/BMI160_Breakout/BMI160_Breakout.ino
@@ -0,0 +1,12 @@
+#include "bmi160_support.h"
+#include "bmi160.h"
+
+void setup() {
+  boolean bmi = bmi160_initialize_sensor(void);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
